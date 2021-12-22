7c5eb3b05b36aaaa27dc9959e2b6ecfd33022e5f
suyeonhong <bellahong95@gmail.com>
2021-12-22 19:40:19 +0900

    디자인 구성 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import './css/style.css';
+import Btns from './components/Btns.js';
+import Panels from './components/Panels.js';
 
 function App() {
   return (
     <div className="App">
+      <h1>TYPOGRAPHY <b>DESGIN</b></h1>
+
+      <Btns />
+
+      <ul className="panel">
+          <Panels />
+      </ul>
     </div>
   );
 }
---
+++ src/components/Btns.js
@@ -0,0 +1,16 @@
+function Btns(){
+    return(
+        <>
+            <a href="#" className="btnUp">
+                <span></span>
+                <p>UP</p>
+            </a>
+            <a href="#" className="btnDown">
+                <span></span>
+                <p>DOWN</p>
+            </a>
+        </>
+    )
+}
+
+export default Btns;
